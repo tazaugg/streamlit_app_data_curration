5541aa41478eec8f345907da8f60b98ce4064378
Tyler Zaugg <tazaugg@gmail.com>
2024-04-11 17:17:09 -0600

    finished

--- democracy_app.py
+++ democracy_app.py
@@ -16,7 +16,32 @@ averages = df_numeric.groupby(df["Democracy classification"]).mean().reset_index
 averages["Country"] = "Average " + averages["Democracy classification"]
 
 df = pd.concat([df, averages], ignore_index=True, sort=False)
-st.title("Democracy Index Dataset Explorer")
+
+st.set_page_config(layout="wide")
+title = """
+    <div style="display: flex; justify-content: center; align-items: center;">
+        <h1>Democracy Index Dataset Explorer</h1>
+    </div>
+"""
+
+st.markdown(title, unsafe_allow_html=True)
+
+cols = st.columns([1, 3, 1])
+
+text = 'This app is designed to help you explore data that contains information about Countries and various metrics of success compared to their level of Democracy'
+text2 = '\n\nIf you would like to learn more about this data and how it was collected I would encourage you to loop through my'
+text3 = ' [repo](https://github.com/tazaugg/data_curation) as well as explore my [data collection blog](https://tazaugg.github.io/blog_practice/2024/03/30/democracy-sucess.html)'
+text4 = '\n\nHave fun exploring the data below!!'
+
+cols[1].markdown(text+text2+text3+text4, unsafe_allow_html=True)
+
+
+
+
+
+
+
+
 st.subheader("Filtered Dataset")
 selected_columns = st.multiselect(
     "Select columns to display", df.columns.tolist()
@@ -35,8 +60,9 @@ sort_by = st.multiselect(
 
 if sort_by:
     for column in sort_by:
-        if column == "Country":
-            filtered_df = filtered_df.sort_values(by=column)
+        ascending = st.radio(f"Sort {column}:", ["High to Low", "Low to High"], key=column)
+        if ascending == "High to Low":
+            filtered_df = filtered_df.sort_values(by=[column], ascending=False)
         else:
             filtered_df = filtered_df.sort_values(by=[column])
 filtered_df = filtered_df.rename(columns={"Country": "Country                                               "})
